a466df9c830cfb2e74099a3df5e8a48ce9f58ac8
emrullahardc <emrullahardc@gmail.com>
2023-10-27 19:29:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ BUILD=202112181
 PASS=$(openssl rand -base64 32|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
 DBPASS=$(openssl rand -base64 24|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
 SERVERID=$(openssl rand -base64 12|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
-REPO=openclassify/pure
+REPO=PurePanel/PurePanel
 
 ####################################################   CLI TOOLS   ###
 reset=$(tput sgr0)
