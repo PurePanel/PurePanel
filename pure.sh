5b382fd14c0b063b95c5978270544a0d2258e447
Fatih Alp <fatihalp89@gmail.com>
2023-12-15 22:43:55 +0300

    Docker and Cleaner

--- pure.sh
+++ pure.sh
@@ -5,13 +5,24 @@ echo "----------------- Pure -------------------------"
 echo "----- Automated install script with Docker -----"
 echo "================================================"
 
+SECONDS=0
+
 echo "$(date)"
 docker --version
 rm -rf vendor
 rm -f composer.lock
-
 cp .env-sail .env
-docker compose restart pure_php
+
+#docker-compose stop database &
+#sleep 15
+#wait
+#docker volume rm purepanel_db-store &
+#wait
+##docker-compose start database &
+#wait
+docker-compose restart php
 
 docker compose up
 echo "$(date)"
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
