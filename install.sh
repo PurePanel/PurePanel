31457f47a22c133a8d675f81027160d48384cde7
Vedat AKDOĞAN <39536659+spektra2147@users.noreply.github.com>
2024-03-22 10:02:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -351,27 +351,7 @@ max_input_time = 180
 EOF
 sudo service php8.1-fpm restart
 
-sudo apt-get -y install php8.2-fpm
-sudo apt-get -y install php8.2-common
-sudo apt-get -y install php8.2-curl
-sudo apt-get -y install php8.2-openssl
-sudo apt-get -y install php8.2-bcmath
-sudo apt-get -y install php8.2-mbstring
-sudo apt-get -y install php8.2-tokenizer
-sudo apt-get -y install php8.2-mysql
-sudo apt-get -y install php8.2-sqlite3
-sudo apt-get -y install php8.2-pgsql
-sudo apt-get -y install php8.2-redis
-sudo apt-get -y install php8.2-memcached
-sudo apt-get -y install php8.2-json
-sudo apt-get -y install php8.2-zip
-sudo apt-get -y install php8.2-xml
-sudo apt-get -y install php8.2-soap
-sudo apt-get -y install php8.2-gd
-sudo apt-get -y install php8.2-imagick
-sudo apt-get -y install php8.2-fileinfo
-sudo apt-get -y install php8.2-imap
-sudo apt-get -y install php8.2-cli
+sudo apt-get -y install php8.2-fpm php8.2-common php8.2-curl php8.2-bcmath php8.2-mbstring php8.2-tokenizer php8.2-mysql php8.2-sqlite3 php8.2-pgsql php8.2-redis php8.2-memcached php8.2-zip php8.2-xml php8.2-soap php8.2-gd php8.2-imagick php8.2-fileinfo php8.2-imap php8.2-cli
 PHPINI=/etc/php/8.2/fpm/conf.d/pure.ini
 sudo touch $PHPINI
 sudo cat > "$PHPINI" <<EOF
